d9327aab3174b367fa097214c029bd80e223ed35
RAM <rebecca.mcginley@gmail.com>
2016-01-09 22:18:45 -0800

    added sendgrid email services

--- friendshipBracelet/routes/contact.js
+++ friendshipBracelet/routes/contact.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var nodemailer = require('nodemailer');
+var sendgrid = require('sendgrid')();
+
 
 var results;
 
@@ -21,46 +22,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/send', function(req,res){
-	// create reusable transporter object using the default SMTP transport
-	var transporter = nodemailer.createTransport({
-		service: 'Gmail',
-		auth: {
-			user: '',
-			pass: ''
-		}
+	var colorChoice = ""
+	if(req.body.Red==''){colorChoice = colorChoice + ' <br>Red'}
+	if(req.body.Orange==''){colorChoice = colorChoice + ' <br>Orange'}
+	if(req.body.Yellow==''){colorChoice = colorChoice + ' <br>Yellow'}
+	if(req.body.Green==''){colorChoice = colorChoice + ' <br>Green'}
+	if(req.body.Blue==''){colorChoice = colorChoice + ' <br>Blue'}
+	if(req.body.Purple==''){colorChoice = colorChoice + ' <br>Purple'}
+	var email = new sendgrid.Email({
+	    to: req.body.email,
+		from: 'rebecca.mcginley@gmail.com',
+		subject: 'You have an order',
+		html: '<p>You have an order! They want.. </p><br>Sender Name: ' + req.body.sender + ' <br>Sender email ' +
+			 req.body.email + '<br>Friend name: ' + req.body.friend + '<br>Address is: <br>' + 
+			 req.body.Address1 + ' ' + req.body.Address2 + '<br>' + req.body.City + ', ' + req.body.State + 
+			 ' ' + req.body.Zip + ' <br>Style: ' + results[req.body.styles]["name"] + '<br>Color selections:' + 
+			 colorChoice + '</ul>'
 	});
 
-	var mailOptions = {
-		from: 'RAM  <Poop@doublebloop.com>', // sender address
-		to: 'rmcginley@illumina.com', // list of receivers
-		subject: 'Order Request', // Subject line
-		text: 'You have an order! They want.. Sender Name: ' + req.body.sender + ' sender email ' +
-		 req.body.email + ' friend name ' + req.body.friend + ' with addres ' + 
-		 req.body.Address1 + ' ' + req.body.Address2 + ' ' + req.body.City + ' ' + req.body.State + 
-		 ' ' + req.body.Zip + ' with style ' + req.body.styles + ' and color selection Red ' + req.body.Red +
-		 + ' Orange ' + req.body.Orange + ' Yellow ' + req.body.Yellow + ' Green ' + req.body.Green +
-		 ' Blue ' + req.body.Blue + ' Purple ' + req.body.Purple,
-		 // plaintext body
-		html: '<p>You have an order! They want.. </p><li>Sender Name: ' + req.body.sender + ' </li><li>Sender email ' +
-		 req.body.email + '</li><li>friend name ' + req.body.friend + '</li><li>with addres </li><li>' + 
-		 req.body.Address1 + '</li><li> ' + req.body.Address2 + '</li><li> ' + req.body.City + ' ' + req.body.State + 
-		 ' ' + req.body.Zip + ' </li><li>with style ' + req.body.styles + ' and color selection </li><li>\Red ' + req.body.Red +
-		 + ' Orange ' + req.body.Orange + ' Yellow ' + req.body.Yellow + ' Green ' + req.body.Green +
-		 ' Blue ' + req.body.Blue + ' Purple ' + req.body.Purple + '</ul>', // html body
-	};
-
-	transporter.sendMail(mailOptions, function(error, info){
-    if(error){
+	sendgrid.send(email, function(error,info){
+	if(error){
         return console.log(error);
         res.redirect('/');
     }
     	console.log('Message sent: ' + info.response);
     	res.redirect('/');
+	});
 
 });
-
-	
-})
-
 
 module.exports = router;
